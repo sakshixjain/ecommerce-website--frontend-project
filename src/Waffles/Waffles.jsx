0782d362c0ad28389d4aa70a85fd37417403a72d
Sakshi Jain <140073552+sakshixjain@users.noreply.github.com>
2024-09-25 21:20:36 +0530

    Update Waffles.jsx

--- src/Waffles/Waffles.jsx
+++ src/Waffles/Waffles.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Waffles() {
   return (
     <div>
-      <h1 className='text-gray-800 text-4xl m-48 text-center font-bold'>Waffles in  the Third Page</h1>
+      <h1 className='text-gray-800 text-4xl m-48 text-left ml-8 font-bold'>Liege Belgian Waffles</h1>
     </div>
   
   )
