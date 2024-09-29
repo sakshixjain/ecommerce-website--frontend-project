6cc0fa579bb0277188f49343e6d8863cf53344fd
Sakshi Jain <140073552+sakshixjain@users.noreply.github.com>
2024-09-29 20:23:35 +0530

    Update Waffles.jsx

--- src/Waffles/Waffles.jsx
+++ src/Waffles/Waffles.jsx
@@ -3,10 +3,20 @@ import React from 'react'
 function Waffles() {
   return (
     <div>
+      <div>
       <h1 className='text-gray-800 text-5xl m-48 text-left ml-8 font-bold'>Liege Belgian Waffles</h1>
       <p className='ml-32 text-lg'>Soft brioche-style Liège Belgian waffles made real butter and imported Belgian pearl sugar for a sweet caramelized crunch in every bite. Just heat and eat.</p>
-        
-  
+        </div>
+         <div className='polard1'>
+     <HoverImage
+            initialSrc="/images/pret.webp"
+            hoverSrc="/images/latest2.webp"
+            className=" rounded-t-2xl object-cover mouseOver3"
+          />
+    <h1 className="container p1">
+      Sweet & Salty super pack <br/> $99.54
+    </h1>
+</div>  
     </div>
   
   )
