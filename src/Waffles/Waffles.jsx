7fe77dd79fec27ab253d7c4f8e6dca40a7449220
Sakshi Jain <140073552+sakshixjain@users.noreply.github.com>
2024-09-29 20:20:16 +0530

    Update Waffles.jsx

--- src/Waffles/Waffles.jsx
+++ src/Waffles/Waffles.jsx
@@ -6,7 +6,7 @@ function Waffles() {
       <h1 className='text-gray-800 text-5xl m-48 text-left ml-8 font-bold'>Liege Belgian Waffles</h1>
       <p className='ml-32 text-lg'>Soft brioche-style Liège Belgian waffles made real butter and imported Belgian pearl sugar for a sweet caramelized crunch in every bite. Just heat and eat.</p>
         
-  </div>
+  
     </div>
   
   )
