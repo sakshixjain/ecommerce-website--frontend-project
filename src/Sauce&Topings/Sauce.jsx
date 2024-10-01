239ddd0816393799e900614036cf6fce922548c4
Sakshi Jain <140073552+sakshixjain@users.noreply.github.com>
2024-10-01 20:59:12 +0530

    Update Sauce.jsx

--- src/Sauce&Topings/Sauce.jsx
+++ src/Sauce&Topings/Sauce.jsx
@@ -4,7 +4,80 @@ function Sauce() {
   return (
     
     <div>
-      <h1 className='text-gray-800 text-4xl m-48 text-center font-bold'>Sauce & Toppings in the Fouth Page</h1>
+      <h1 className='text-gray-800 text-5xl mt-48 ml-20 text-left justify-center items-center font-bold' >Sauces & Toppings</h1>
+      <p className='ml-20 text-lg mt-4'>Artfully blended gourmet sauces and toppings.</p>
+      <div className='box-sauce -mt-40'>
+        <div id='image-sauce'>
+      <div className="box-border h-[320px] w-[310px] rounded-3xl text-center imag "
+        >
+          <fieldset>
+            <img
+              src="/images/sauce1.webp"
+              className="h-[260px] w-[280px] rounded-3xl"
+            />
+          </fieldset>
+          <h1 className="text-xl text-blue-950 font-semibold  sauce p-5">Maui Onion Mustard</h1>
+        </div>
+        <div 
+          className="box-border h-[320px] w-[310px] rounded-3xl text-center imag"
+        >
+          <fieldset>
+            <img
+              src="/images/sauce2.webp"
+              className="h-[260px] w-[280px] rounded-3xl"
+            />
+          </fieldset>
+          <h1 className="text-xl text-blue-950 font-semibold  sauce p-5">Maui Onion Mustard</h1>
+        
+        </div>
+        <div 
+          className="box-border h-[320px] w-[310px] rounded-3xl text-center imag"
+        >
+          <fieldset>
+            <img
+              src="/images/sauce2.webp"
+              className="h-[260px] w-[280px] rounded-3xl"
+            />
+          </fieldset>
+          <h1 className="text-xl text-blue-950 font-semibold  sauce p-5">Maui Onion Mustard</h1>
+        
+        </div>
+        <div 
+          className="box-border h-[320px] w-[310px] rounded-3xl text-center imag"
+        >
+          <fieldset>
+            <img
+              src="/images/sauce3.webp"
+              className="h-[260px] w-[280px] rounded-3xl"
+            />
+          </fieldset>
+          <h1 className="text-xl text-blue-950 font-semibold  sauce p-5">Maui Onion Mustard</h1>
+        
+        </div>
+        <div 
+          className="box-border h-[320px] w-[310px] rounded-3xl text-center imag"
+        >
+          <fieldset>
+            <img
+              src="/images/sauce5.webp"
+              className="h-[260px] w-[280px] rounded-3xl"
+            />
+          </fieldset>
+          <h1 className="text-xl text-blue-950 font-semibold  sauce p-5">Maui Onion Mustard</h1>   
+        </div>
+        <div 
+          className="box-border h-[320px] w-[310px] rounded-3xl text-center imag"
+        >
+          <fieldset>
+            <img
+              src="/images/sauce6.webp"
+              className="h-[260px] w-[280px] rounded-3xl"
+            />
+          </fieldset>
+          <h1 className="text-xl text-blue-950 font-semibold  sauce p-5">Maui Onion Mustard</h1>      
+        </div>
+</div>
+      </div>
     </div>
   )
 }
