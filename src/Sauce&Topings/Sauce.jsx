66a35e4ded2b6d6de6ed6cf4cd5e35c94dc2cca5
Sakshi Jain <140073552+sakshixjain@users.noreply.github.com>
2024-10-02 17:19:06 +0530

    Update Sauce.jsx

--- src/Sauce&Topings/Sauce.jsx
+++ src/Sauce&Topings/Sauce.jsx
@@ -8,73 +8,73 @@ function Sauce() {
       <p className='ml-20 text-lg mt-4'>Artfully blended gourmet sauces and toppings.</p>
       <div className='box-sauce -mt-40'>
         <div id='image-sauce'>
-      <div className="box-border h-[320px] w-[310px] rounded-3xl text-center imag "
+      <div className="box-border h-[310px] w-[310px] rounded-3xl text-center imag "
         >
           <fieldset>
             <img
               src="/images/sauce1.webp"
-              className="h-[260px] w-[280px] rounded-3xl"
+              className="h-[250px] w-[270px] rounded-3xl"
             />
           </fieldset>
-          <h1 className="text-xl text-blue-950 font-semibold  sauce p-5">Maui Onion Mustard</h1>
+          <h1 className="text-xl -mt-4 text-left text-blue-950 font-semibold  sauce p-5">Maui Onion Mustard</h1>
         </div>
         <div 
-          className="box-border h-[320px] w-[310px] rounded-3xl text-center imag"
+          className="box-border h-[310px] w-[310px] rounded-3xl text-center imag"
         >
           <fieldset>
             <img
               src="/images/sauce2.webp"
-              className="h-[260px] w-[280px] rounded-3xl"
+              className="h-[250px] w-[270px] rounded-3xl"
             />
           </fieldset>
-          <h1 className="text-xl text-blue-950 font-semibold  sauce p-5">Maui Onion Mustard</h1>
+          <h1 className="text-xl -mt-4 text-left text-blue-950 font-semibold  sauce p-5">White Cheddar Cheese Sauce</h1>
         
         </div>
         <div 
-          className="box-border h-[320px] w-[310px] rounded-3xl text-center imag"
-        >
-          <fieldset>
-            <img
-              src="/images/sauce2.webp"
-              className="h-[260px] w-[280px] rounded-3xl"
-            />
-          </fieldset>
-          <h1 className="text-xl text-blue-950 font-semibold  sauce p-5">Maui Onion Mustard</h1>
-        
-        </div>
-        <div 
-          className="box-border h-[320px] w-[310px] rounded-3xl text-center imag"
+          className="box-border h-[310px] w-[310px] rounded-3xl text-center imag"
         >
           <fieldset>
             <img
               src="/images/sauce3.webp"
-              className="h-[260px] w-[280px] rounded-3xl"
+              className="h-[250px] w-[270px] rounded-3xl"
             />
           </fieldset>
-          <h1 className="text-xl text-blue-950 font-semibold  sauce p-5">Maui Onion Mustard</h1>
+          <h1 className="text-xl -mt-4 text-left text-blue-950 font-semibold  sauce p-5">French Toast Sugar</h1>
         
         </div>
         <div 
-          className="box-border h-[320px] w-[310px] rounded-3xl text-center imag"
+          className="box-border h-[310px] w-[310px] rounded-3xl text-center imag"
+        >
+          <fieldset>
+            <img
+              src="/images/sauce4.webp"
+              className="h-[250px] w-[270px] rounded-3xl"
+            />
+          </fieldset>
+          <h1 className="text-xl -mt-4 text-left text-blue-950 font-semibold  sauce p-5">Peanut Butter Caramel Sauce</h1>
+        
+        </div>
+        <div 
+          className="box-border h-[310px] w-[310px] rounded-3xl text-center imag"
         >
           <fieldset>
             <img
               src="/images/sauce5.webp"
-              className="h-[260px] w-[280px] rounded-3xl"
+              className="h-[250px] w-[270px] rounded-3xl"
             />
           </fieldset>
-          <h1 className="text-xl text-blue-950 font-semibold  sauce p-5">Maui Onion Mustard</h1>   
+          <h1 className="text-xl -mt-4 text-left text-blue-950 font-semibold sauce p-5">Chilli Lime Pretzel Sauce</h1>   
         </div>
         <div 
-          className="box-border h-[320px] w-[310px] rounded-3xl text-center imag"
+          className="box-border h-[310px] w-[310px] rounded-3xl text-center imag"
         >
           <fieldset>
             <img
               src="/images/sauce6.webp"
-              className="h-[260px] w-[280px] rounded-3xl"
+              className="h-[250px] w-[270px] rounded-3xl"
             />
           </fieldset>
-          <h1 className="text-xl text-blue-950 font-semibold  sauce p-5">Maui Onion Mustard</h1>      
+          <h1 className="text-xl -mt-4 text-left text-blue-950 font-semibold  sauce p-5">Sweet Love Sprinkles</h1>      
         </div>
 </div>
       </div>
