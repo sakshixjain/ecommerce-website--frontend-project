5adc0b1dadf49c9788094ea7a92f77265c31a153
Sakshi Jain <140073552+sakshixjain@users.noreply.github.com>
2024-10-02 17:17:07 +0530

    Update Gift.jsx

--- src/Gift Boxes/Gift.jsx
+++ src/Gift Boxes/Gift.jsx
@@ -3,9 +3,81 @@ import React from 'react'
 function Gift() {
   return (
     <div>
-           <div className=' rounded-xl mx-17 text-xs right-0 fixed top-50 bottom-9 '> <button className="shop4"><i className="fa-brands fa-rocketchat"></i>Chat with us</button></div>
-     <h1 className='text-gray-800 text-4xl m-48 text-center font-bold'>Gift and Boxes in  the Fifth Page</h1> 
+    <h1 className='text-gray-800 text-5xl mt-48 ml-20 text-left justify-center items-center font-bold' >Gift Boxes</h1>
+    <p className='ml-20 text-lg mt-4'>The Perfect for any Occasion</p>
+    <div className='box-sauce -mt-40'>
+      <div id='image-sauce'>
+    <div className="box-border h-[320px] w-[310px] rounded-3xl text-center imag "
+      >
+        <fieldset>
+          <img
+            src="/images/giftbox1.webp"
+            className="h-[250px] w-[270px] rounded-3xl"
+          />
+        </fieldset>
+        <h1 className="text-xl -mt-4 text-left text-blue-950 font-semibold  sauce p-5">Happy Birthday</h1>
+      </div>
+      <div 
+        className="box-border h-[320px] w-[310px] rounded-3xl text-center imag"
+      >
+        <fieldset>
+          <img
+            src="/images/giftbox2.webp"
+            className="h-[250px] w-[270px] rounded-3xl"
+          />
+        </fieldset>
+        <h1 className="text-xl -mt-4 text-left text-blue-950 font-semibold  sauce p-5">Thankyou</h1>
+      
+      </div>
+      <div 
+        className="box-border h-[320px] w-[310px] rounded-3xl text-center imag"
+      >
+        <fieldset>
+          <img
+            src="/images/giftbox3.webp"
+            className="h-[250px] w-[270px] rounded-3xl"
+          />
+        </fieldset>
+        <h1 className="text-xl -mt-4 text-left text-blue-950 font-semibold  sauce p-5">Congratulations</h1>
+      
+      </div>
+      <div 
+        className="box-border h-[320px] w-[310px] rounded-3xl text-center imag"
+      >
+        <fieldset>
+          <img
+            src="/images/giftbox4.webp"
+            className="h-[250px] w-[270px] rounded-3xl"
+          />
+        </fieldset>
+        <h1 className="text-xl -mt-4 text-left text-blue-950 font-semibold  sauce p-5">Any Occasion</h1>
+      
+      </div>
+      <div 
+        className="box-border h-[320px] w-[310px] rounded-3xl text-center imag"
+      >
+        <fieldset>
+          <img
+            src="/images/giftbox5.webp"
+            className="h-[250px] w-[270px] rounded-3xl"
+          />
+        </fieldset>
+        <h1 className="text-xl -mt-4 text-left text-blue-950 font-semibold  sauce p-5">Just For fun</h1>   
+      </div>
+      <div 
+        className="box-border h-[320px] w-[310px] rounded-3xl text-center imag"
+      >
+        <fieldset>
+          <img
+            src="/images/giftbox6.webp"
+            className="h-[250px] w-[270px] rounded-3xl"
+          />
+        </fieldset>
+        <h1 className="text-xl -mt-4 text-left text-blue-950 font-semibold  sauce p-5">Oprah's Favorite Things</h1>      
+      </div>
+</div>
     </div>
+  </div>
   )
 }
 
