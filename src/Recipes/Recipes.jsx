582f96ac750d169980f7a539471887afa965ae56
Sakshi Jain <140073552+sakshixjain@users.noreply.github.com>
2024-10-03 17:31:54 +0530

    Update Recipes.jsx

--- src/Recipes/Recipes.jsx
+++ src/Recipes/Recipes.jsx
@@ -4,7 +4,146 @@ function Recipes() {
   return (
     
     <div>
-      <h1 className='text-gray-800 text-4xl m-48 text-center font-bold'>Recipes in  the Sixth page</h1>
+      <h1 className='text-gray-800 text-5xl  mt-32 text-center font-serif font-extrabold'>GET YOUR TWIST ON</h1>
+    <p className='ml-20 text-xl font-semibold hovmt-4 '>Reciepes </p>
+    <p className='ml-20 text-xl font-semibold hovmt-4'>Lifestyles</p>
+    <div className='box-sauce w-[1200px] '>
+      <div id='image-sauce'>
+    <div className="box-border h-[300px] w-[460px] mb-80 rounded-3xl text-center imag"
+      >
+        <fieldset>
+          <img
+            src="/images/recipe1.webp"
+            className="h-[300px] w-[460px] rounded-3xl "
+          />
+        </fieldset>
+        <h1 className="text-xl text-left mt-5 ml-2 relative">
+              <i className="fa-solid fa-cake-candles"></i> 25 September, 2024
+            </h1>
+            <br />
+            <h1 className="text-3xl font-bold hov text-slate-800 text-left  relative " >
+            Pumpkin Marshmallow Espresso Martini
+            </h1>
+            <br />
+            <h1 className="text-[20px] overflow-hidden text-ellipsis relative">
+            Directions Place pumpkin, marshmallow in glass, make espresso over and stir. In shaker with ice, pour over mixed flavored espresso, vodka and shake. Rim
+            </h1>
+      </div>
+      <div 
+        className="box-border h-[300px] w-[460px] mb-80 rounded-3xl text-center imag"
+      >
+        <fieldset>
+          <img
+            src="/images/recipe22.webp"
+            className="h-[300px] w-[460px] rounded-3xl"
+          />
+        </fieldset>
+        <h1 className="text-xl text-left mt-5 ml-2 relative">
+              <i className="fa-solid fa-cake-candles"></i> 25 September, 2024
+            </h1>
+            <br />
+            <h1 className="text-3xl font-bold hov text-slate-800 text-left  relative " >
+            Caramel Pretzel Apple Bark
+            </h1>
+            <br />
+            <h1 className="text-[20px] overflow-hidden text-ellipsis relative">
+              For the whipped cream: 1 1/2 cups heavy cream 6 Tbsp sugar 1 
+              tsp vanilla extract For the Pudding Layer (optional): 4 oz white 
+              chocolate instant pudding mix(dry) 2 cups cold milk For the Trifl
+            </h1>
+      
+      </div>
+      <div 
+        className="box-border h-[300px] w-[460px] mb-80 rounded-3xl text-center imag"
+      >
+        <fieldset>
+          <img
+            src="/images/recipe3.webp"
+            className="h-[300px] w-[460px] rounded-3xl"
+          />
+        </fieldset>
+        <h1 className="text-xl text-left mt-5 ml-2 relative">
+              <i className="fa-solid fa-cake-candles"></i> 25 September, 2024
+            </h1>
+            <br />
+            <h1 className="text-3xl font-bold hov text-slate-800 text-left  relative " >
+            Apple Cider Margarita
+            </h1>
+            <br />
+            <h1 className="text-[20px] overflow-hidden text-ellipsis relative">
+            Directions Place pumpkin, marshmallow in glass, make espresso over and stir. In shaker with ice, pour over mixed flavored espresso, vodka and shake. Rim
+            </h1>
+      
+      </div>
+      <div 
+        className="box-border h-[300px] w-[460px] mb-80 rounded-3xl text-center imag"
+      >
+        <fieldset>
+          <img
+            src="/images/recipe4.webp"
+            className="h-[300px] w-[460px] rounded-3xl"
+          />
+        </fieldset>
+        <h1 className="text-xl text-left mt-5 ml-2 relative">
+              <i className="fa-solid fa-cake-candles"></i> 25 September, 2024
+            </h1>
+            <br />
+            <h1 className="text-3xl font-bold hov text-slate-800 text-left  relative " >
+            Italian Sweet & Savory Charcuterie Board
+            </h1>
+            <br />
+            <h1 className="text-[20px] overflow-hidden text-ellipsis relative">
+              For the whipped cream: 1 1/2 cups heavy cream 6 Tbsp sugar 1 
+              tsp vanilla extract For the Pudding Layer (optional): 4 oz white 
+              chocolate instant pudding mix(dry) 2 cups cold milk For the Trifl
+            </h1>
+      
+      </div>
+      <div 
+        className="box-border h-[300px] w-[460px] mb-80 rounded-3xl text-center imag"
+      >
+        <fieldset>
+          <img
+            src="/images/recipe5.webp"
+            className="h-[300px] w-[460px] rounded-3xl"
+          />
+        </fieldset>
+        <h1 className="text-xl text-left mt-5 ml-2 relative">
+              <i className="fa-solid fa-cake-candles"></i> 25 September, 2024
+            </h1>
+            <br />
+            <h1 className="text-3xl font-bold hov text-slate-800 text-left  relative " >
+            Italian Baked Cheese Dip
+            </h1>
+            <br />
+            <h1 className="text-[20px] overflow-hidden text-ellipsis relative">
+            Instructions: Preheat oven to 400°F. Coat small (about 2½ cup) oven-proof baking dish with cooking spray. Beat cream cheese in a large bowl with electric mixer until light and airy, about 2 minutes. Add 1/2 cup
+            </h1>   
+      </div>
+      <div 
+        className="box-border h-[300px] w-[460px] mb-80 rounded-3xl text-center imag"
+      >
+        <fieldset>
+          <img
+            src="/images/recipe6.webp"
+            className="h-[300px] w-[460px] rounded-3xl"
+          />
+        </fieldset>
+        <h1 className="text-xl text-left mt-5 ml-2 relative">
+              <i className="fa-solid fa-cake-candles"></i> 25 September, 2024
+            </h1>
+            <br />
+            <h1 className="text-3xl font-bold hov text-slate-800 text-left relative " >
+            Cannoli Dip
+            </h1>
+            <br />
+            <h1 className="text-[20px] overflow-hidden text-ellipsis relative">
+            Instructions: Preheat oven to 400°F. Coat small (about 2½ cup) oven-proof baking dish with cooking spray. Beat cream cheese in a large bowl with electric mixer until light and airy, about 2 minutes. Add 1/2 cup
+            </h1>    
+      </div>
+</div>
+    </div>
+
     </div>
   )
 }
